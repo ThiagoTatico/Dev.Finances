63e3e4a56460c5e0cf83a926e43147915ba3d7ef
Ariun <thiagotatico007@gmail.com>
2022-02-07 10:28:50 -0300

    changes in JS file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Modal = {
   open() {
+    // ADD class "active" to the modalOverlay
     document.querySelector('.modalOverlay').classList.add('active')
   },
 
   close() {
+    // REMOVE class "active" to the modalOverlay
     document.querySelector('.modalOverlay').classList.remove('active')
   }
 }
