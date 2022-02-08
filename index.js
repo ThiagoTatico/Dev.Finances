b7479da54b96e6e4894c354af67ed4032d46c748
Ariun <thiagotatico007@gmail.com>
2022-02-08 15:30:10 -0300

    Update JS functionalities

--- index.js
+++ index.js
@@ -6,23 +6,7 @@ const Modal = {
 }
 
 const Transaction = {
-  all: [
-    {
-      description: 'Luz',
-      amount: -50000,
-      date: '23/01/2021'
-    },
-    {
-      description: 'Website',
-      amount: 500000,
-      date: '23/01/2021'
-    },
-    {
-      description: 'Internet',
-      amount: -20000,
-      date: '23/01/2021'
-    }
-  ],
+  all: [],
   
   add(transaction) {
     Transaction.all.push(transaction)
@@ -68,11 +52,13 @@ const DOM = {
 
   addTransaction(transaction, index) {
     const tr = document.createElement('tr')
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+    tr.dataset.index = index
+
     DOM.transactionsContainer.appendChild(tr)
   },
 
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
 
     const amount = Utils.formatCurrency(transaction.amount)
@@ -81,7 +67,7 @@ const DOM = {
       <td class="description">${transaction.description}</td>
       <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
-      <td> <img src="./images/minus.svg" alt="Minus Signal"> </td>
+      <td> <img onclick="Transaction.remove(${index})" src="./images/minus.svg" alt="Minus Signal"> </td>
     `
     return html
   },
@@ -98,6 +84,17 @@ const DOM = {
 }
 
 const Utils = {
+  formatAmount(value) {
+    value = Number(value) * 100
+
+    return value
+  },
+
+  formatDate(date) {
+    const splittedDate = date.split('-')
+    return `${splittedDate[1]}/${splittedDate[2]}/${splittedDate[0]}`
+  },
+
   formatCurrency(value) {
     const signal = Number(value) < 0 ? '-' : ''
 
@@ -128,11 +125,30 @@ const Form = {
   },
 
   validateFields() {
-    const { description, amount, date } = Form.getValues(),
+    const { description, amount, date } = Form.getValues()
 
     if (description.trim() === '' || amount.trim() === '' || date.trim() === '') {
       throw new Error('Please fill in all fields')
     }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues()
+
+    amount = Utils.formatAmount(amount)
+    date = Utils.formatDate(date)
+
+    return {
+      description,
+      amount,
+      date
+    }
+  },
+
+  clearFields() {
+    Form.description.value = ''
+    Form.amount.value = ''
+    Form.date.value = ''
   },
 
   submit(event) {
@@ -140,20 +156,19 @@ const Form = {
 
     try{
       Form.validateFields()
-      
+      const transaction = Form.formatValues()
+      Transaction.add(transaction)
+      Form.clearFields()
+      Modal.showModal()
     } catch(error) {
       alert(error.message)
     }
-
   }
 }
 
 const App = {
   init() {
-    Transaction.all.forEach(transaction => {
-      DOM.addTransaction(transaction)
-    })
-
+    Transaction.all.forEach(DOM.addTransaction)
     DOM.updateBalance()
   },
 
